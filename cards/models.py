c1ec988ac5307e2e912bb1922e1063aba4d07c5b
Matt Ward <matthew3.ward@umconnect.umt.edu>
2020-10-16 23:04:13 -0600

    Added image functionality for the media cards

--- cards/models.py
+++ cards/models.py
@@ -6,9 +6,11 @@ from django.urls import reverse
 class Card(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
+    cover = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('card_detail', args=[str(self.id)])
+        
