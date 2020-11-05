238aad0c4509e4795aec1d1129943def081ab596
Matt Ward <matthew3.ward@umconnect.umt.edu>
2020-11-05 15:27:27 -0700

    New updates for Heroku deployment

--- mediaTracker_project/settings.py
+++ mediaTracker_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h88p^c-1uo7)r9!nmg!ac=of=i^_*rj(i#64z=l@vub1!lx247'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
